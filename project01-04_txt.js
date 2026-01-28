79beae719801deca34fe9bae6792ee2102583c00
joshuagdd <joshuagabrielledomingo@gmail.com>
2026-01-29 03:09:40 +0800

    Remove commented header and clean up code

--- project01-04_txt.js
+++ project01-04_txt.js
@@ -1,15 +1,3 @@
-/*  JavaScript 7th Edition
-    Chapter 1
-    Hands-On Project 1-4
-
-    Author: 
-    Date:   
-
-    Filename: project01-04.js
-*/
-
-//define variables for home and work addresses
-
 "use strict";
 
 /* Home address */
@@ -48,4 +36,5 @@ workOption.onclick = function () {
   state.value = workState;
   code.value = workCode;
 };
+
 
